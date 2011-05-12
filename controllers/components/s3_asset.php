97c4596ee0427d8bc862d9e78ec7ed87e1673537
Brian Morton <bmorton@sdreader.com>
2011-05-12 02:02:02 -0700

    Abstract out a core class that the component can extend to make code more DRY.

--- controllers/components/s3_asset.php
+++ controllers/components/s3_asset.php
@@ -7,36 +7,8 @@
  * @package default
  * @author Brian Morton
  */
-class S3AssetComponent extends Object {
-
-/**
- * AWS S3 connection object
- *
- * @var object
- */
-	var $s3 = null;
-
-/**
- * AWS Account Key
- *
- * @var string
- */
-	var $key = null;
-
-/**
- * AWS Account Secret
- *
- * @var string
- */
-	var $secretKey = null;
-
-/**
- * Default bucket to use if one isn't specified.
- *
- * @var string
- */
-	var $defaultBucket = null;
-
+App::import('Lib', 'S3.CakeS3');
+class S3AssetComponent extends CakeS3 {
 /**
  * Component initializer that gets called before Controller::beforeFilter()
  *
@@ -72,97 +44,6 @@ class S3AssetComponent extends Object {
  */
 	function startup(&$controller) {
 		
-	}
-
-/**
- * Handle creating an S3 object if it hasn't been created yet.
- * Pass back a valid object to the requester.
- *
- * @author Brian Morton
- * @return object
- */
-	function &connection() {
-		// If key & secretKey weren't defined, we can't do anything.
-		if (is_null($this->key) || is_null($this->secretKey)) {
-			throw new Exception("Can't establish a connection to S3 because the key and secretKey are null.");
-		}
-		
-		// If the S3 object hasn't been created yet, load it here (lazy loading ftw)
-		if (!$this->s3) {
-			App::import('Vendor', 'AWS', array('file' => 'aws' . DS . 'sdk.class.php', 'plugin' => 'S3'));
-			$this->s3 = new AmazonS3($this->key, $this->secretKey);
-		}
-		
-		return $this->s3;
-	}
-	
-/**
- * Wrapper for listing buckets on S3
- *
- * @param string $pcre An optional regular expression for matching bucket names.
- * @return array An array of the buckets that exist on S3
- * @author Brian Morton
- */
-	function getBucketList($pcre = null) {
-		return $this->connection()->get_bucket_list($pcre);
-	}
-
-/**
- * Wrapper for creating objects in an S3 bucket
- *
- * @param array $arguments	An array of arguments that must include a filename and a bucket if the
- *							default is not set or shouldn't be used.  Either body or fileUpload 
- *							must also be set so we know what to send to S3.  Optionally, an options
- *							array argument can be set that conforms to $opt on:
- *							http://docs.amazonwebservices.com/AWSSDKforPHP/latest/#m=AmazonS3/create_object
- * @return object CFResponse
- * @author Brian Morton
- */
-	function createObject($arguments = array()) {
-		$defaults = array(
-			'bucket' => $this->defaultBucket
-		);
-		
-		extract(array_merge($defaults, $arguments));
-		
-		if (isset($body)) {
-			$options['body'] = $body;
-		}
-		
-		if (isset($fileUpload)) {
-			$options['fileUpload'] = $fileUpload;
-		}
-		
-		return $this->connection()->create_object($bucket, $filename, $options);
-	}
-
-/**
- * Gets the web-accessible URL for the Amazon S3 object or generates a time-limited signed request for a private file.
- *
- * @param array $arguments	An array of arguments that must contain:
- *								* filename
- *								* bucket (optional if defaultBucket specified)
- *
- *							And can optionally contain:
- *								* preauth - Specifies that a presigned URL for this request should be returned. May be
- *									passed as a number of seconds since UNIX Epoch, or any string compatible with
- *									strtotime().
- *								* options (array) - must conform to $opt on:
- *									http://docs.amazonwebservices.com/AWSSDKforPHP/latest/#m=AmazonS3/get_object_url
- * @return void
- * @author Brian Morton
- */
-	function getObjectUrl($arguments = array()) {
-		$defaults = array(
-			'bucket' => $this->defaultBucket,
-			'preauth' => 0,
-			'options' => null,
-			'filename' => null
-		);
-		
-		extract(array_merge($defaults, $arguments));
-		
-		return $this->connection()->get_object_url($bucket, $filename, $preauth, $options);
 	}
 
 /**
